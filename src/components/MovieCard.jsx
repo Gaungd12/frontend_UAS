ceee264486ebd64777f2290e3cbab98ecf99bf05
Gaung <241131014@mhs.stiki.ac.id>
2026-01-07 10:47:41 +0700

    first commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,21 @@
 export default function MovieCard({ movie }) {
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.image}`;
+
+  const shortDescription =
+    movie.description.length > 120
+      ? movie.description.slice(0, 120) + "..."
+      : movie.description;
+
   return (
     <div className="movie-card">
+      <img src={movie.image} alt={movie.title} />
       <h3>{movie.title}</h3>
-      <p>{movie.description}</p>
-      <p>🎬 {movie.release_date}</p>
-      <p>⭐ {movie.rt_score}</p>
+      <p className="desc">{shortDescription}</p>
+
+      <div className="info">
+        <span>🎬 {movie.release_date}</span>
+        <span>⭐ {movie.rt_score}</span>
+      </div>
     </div>
   );
 }
